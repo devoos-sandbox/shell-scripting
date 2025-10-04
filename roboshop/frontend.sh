5fed3c4801298c8a8f930938f56a40dffce8369d
CodingManoj <CodingManoj@gmail.com>
2025-10-04 16:05:25 -0400

    Automating Roboshop

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -22,7 +22,7 @@ unzip /tmp/frontend.zip &>> /tmp/frontend.log
 
 rm -f /etc/nginx/nginx.conf &>> /tmp/frontend.log
 
-cp ./nginx.conf /etc/nginx/default.d/nginx.conf 
+curl https://raw.githubusercontent.com/devoos-sandbox/shell-scripting/refs/heads/main/roboshop/nginx.conf >  /etc/nginx/default.d/nginx.conf 
 
 systemctl restart nginx 
 
