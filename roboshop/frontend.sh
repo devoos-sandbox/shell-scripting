64e63906483e35c9e8450547f2111db0472ef25d
CodingManoj <CodingManoj@gmail.com>
2025-10-04 18:41:19 -0400

    Automating Roboshop

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -44,7 +44,7 @@ unzip /tmp/${component}.zip &>> ${appLog}
 rm -f /etc/nginx/nginx.conf &>> ${appLog}
 
 echo -e -n  "\e[33m Updating nginx Proxy: \e[0m"
-curl -sS --fail ${url} -o /etc/nginx/nginx.conf &>> ${appLog}
+curl -s ${url} &> /etc/nginx/nginx.conf &>> ${appLog}
 stat $?
 
 echo -e -n  "\e[33m Updating ${component} Configuration: \e[0m"
