c311a613eb91f3ac644a6bdc20b8e01a721d87e6
CodingManoj <CodingManoj@gmail.com>
2025-10-05 15:29:05 -0400

    Automating Roboshop

---
+++ roboshop/rabbitmq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+stat() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m Success \e[0m"
+  else
+    echo -e "\e[31m Failure \e[0m"
+    echo -e "\e[33m Check the log file /tmp/${component}.log for more information \e[0m"
+    exit 1
+  fi
+}
+
+if [ $(id -u) -ne 0 ]; then
+  echo -e "\e[31m You should be running this script as root or with sudo privileges \e[0m"
+  exit 1
+fi
+
+component="rabbitmq"
+appContent="https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip"
+appLog="/tmp/${component}.log"
+appUser="roboshop"
