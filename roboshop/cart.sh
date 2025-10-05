ac46792cf16c798173e79d65fe136626492e07f5
CodingManoj <CodingManoj@gmail.com>
2025-10-05 14:42:32 -0400

    Automating Roboshop

---
+++ roboshop/cart.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+stat() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m Success \e[0m"
+  else
+    echo -e "\e[31m Failure \e[0m"
+    echo -e "\e[33m Check the log file /tmp/${component}.log for more information \e[0m"
+    exit 1
+  fi
+}
+
+component="cart"
+appContent="https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip"
+appLog="/tmp/${component}.log"
+appUser="roboshop"
+
+echo -e -n "\e[33m Disabling default nodeVersion : \e[0m"
+dnf module disable nodejs -y || true &>> ${appLog}
+dnf module enable nodejs:20 -y || true &>> ${appLog}
+stat $?
+
+echo -e -n "\e[33m Installing nodejs: \e[0m"
+dnf install nodejs -y &>> ${appLog}
+stat $?
+
+echo -e -n "\e[33m Creating AppUser: \e[0m"
+id ${appUser} &>> ${appLog}
+if [ $? -ne 0 ]; then
+  useradd ${appUser} &>> ${appLog}
+  stat $?
+else
+  echo -e "\e[32m ${appUser} user already exists: SKIPPING \e[0m"
+fi
+
+echo -e -n "\e[33m Cleanup of app directory: \e[0m"
+rm -rf /app &>> ${appLog}
+stat $?
+
+echo -e -n "\e[33m Creating App Directory: \e[0m"
+mkdir /app &>> ${appLog}
+stat $?
+
+echo -e -n "\e[33m Configuring ${component} systemd: \e[0m"
+cp "$(dirname "$0")/${component}.service" /etc/systemd/system/${component}.service
+stat $?
+
+echo -e -n "\e[33m Downloading ${component} content: \e[0m"
+curl -sS --fail -o /tmp/${component}.zip ${appContent} &>> ${appLog}
+cd /app
+unzip -o /tmp/${component}.zip &>> ${appLog}
+stat $?
+
+echo -e -n "\e[33m Installing nodejs dependencies: \e[0m"
+cd /app
+npm install &>> ${appLog}
+stat $?
+
+echo -e -n "\e[33m Starting ${component}: \e[0m"
+systemctl daemon-reload &>> ${appLog}
+systemctl enable ${component} &>> ${appLog}
+systemctl start ${component} &>> ${appLog}
+stat $?
+
+echo -e "\n\n\t\e[32m ${component} setup completed \e[0m"
+echo -e "\e[36m------------------------------------------------------\e[0m"
+echo -e "\e[36m Service Name   : ${component}\e[0m"
+echo -e "\e[36m Log File       : ${appLog}\e[0m"
+echo -e "\e[36m App Directory  : /app\e[0m"
+echo -e "\e[1;33mTo manage the ${component} service, use: systemctl [start|stop|status] ${component}\e[0m"
+echo -e "\e[1;33mCheck the log file /tmp/${component}.log for more details.\e[0m" 
+echo -e "\e[36m------------------------------------------------------\e[0m"
